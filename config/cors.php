78a2c61392f40a9c10711156a00c903be76cdf4e
Kpono Akpabio <kpono.akpabio@sbtelecoms.com>
2018-08-31 13:32:48 +0100

    added cors library

---
+++ config/cors.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MY-PC
+ * Date: 8/31/2018
+ * Time: 7:04 AM
+ */
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS
+    |--------------------------------------------------------------------------
+    |
+    | allowedOrigins, allowedHeaders and allowedMethods can be set to array('*')
+    | to accept any value.
+    |
+    */
+    'supportsCredentials' => false,
+    'allowedOrigins' => ['*'],
+    'allowedHeaders' => ['Content-Type', 'X-Requested-With','Authorization'],
+    'allowedMethods' => ['*'], // ex: ['GET', 'POST', 'PUT',  'DELETE']
+    'exposedHeaders' => [],
+    'maxAge' => 0,
+];
