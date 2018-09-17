7b84f46d4d739262b8cbec40962d8087b94f1c8d
Kpono Akpabio <kpono.akpabio@sbtelecoms.com>
2018-09-17 11:12:04 +0100

    added support ticket

--- config/app.php
+++ config/app.php
@@ -151,8 +151,6 @@ return [
          * Package Service Providers...
          */
 
-        Intervention\Image\ImageServiceProvider::class,
-
         /*
          * Application Service Providers...
          */
@@ -209,8 +207,7 @@ return [
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class,
+        'View' => Illuminate\Support\Facades\View::class
 
 
     ],
