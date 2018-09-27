118c16d68816a8a6a24a26f61c52d7edb23a6a97
Kpono Akpabio <kpono.akpabio@sbtelecoms.com>
2018-09-27 15:29:59 +0100

    sorted client controller bug

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -15,7 +15,7 @@ class Client extends Model
      * @var array
      */
     protected $fillable = [
-        'id','client_type','service_type-id','vendor_status','sector_id','name','contact_person','mobile','bdm_person_id', 'email',
+        'id','client_type','service_type_id','vendor_status','sector_id','name','contact_person','mobile','bdm_person_id', 'email',
          'address'
     ];
 
